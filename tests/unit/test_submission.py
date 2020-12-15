224c3f890396d218caca93866b478702162ca6f7
Hernan Tain <htain@trustar.co>
2020-12-15 16:06:23 -0300

    Fixing import

--- tests/unit/test_submission.py
+++ tests/unit/test_submission.py
@@ -7,12 +7,14 @@ from trustar.submission import Submission
 from trustar.trustar import TruStar
 from trustar.models import Indicator, Entity
 
-from resources import submission_example_request
+from tests.unit.resources import submission_example_request
 
 
 @pytest.fixture
 def submission():
-    return Submission(TruStar(api_key="xxxx", api_secret="xxx", client_metatag="test_env"))
+    return Submission(
+        TruStar(api_key="xxxx", api_secret="xxx", client_metatag="test_env")
+    )
 
 
 @pytest.fixture
@@ -21,13 +23,15 @@ def indicators():
     related_observable_email = Entity.observable("EMAIL_ADDRESS", "bob@gmail.com")
     related_observable_bad_url = Entity.observable("URL", "badurl.com")
     return [
-        Indicator("IP4", "1.2.3.4").set_malicious_score("HIGH")
-                                   .set_attributes(bad_panda)
-                                   .set_related_observables(related_observable_email),
-        Indicator("IP4", "5.6.7.8").set_malicious_score("HIGH")
-                                   .set_attributes(bad_panda)
-                                   .set_related_observables(related_observable_bad_url)
-                                   .set_tags(["TAG1"])
+        Indicator("IP4", "1.2.3.4")
+        .set_malicious_score("HIGH")
+        .set_attributes(bad_panda)
+        .set_related_observables(related_observable_email),
+        Indicator("IP4", "5.6.7.8")
+        .set_malicious_score("HIGH")
+        .set_attributes(bad_panda)
+        .set_related_observables(related_observable_bad_url)
+        .set_tags(["TAG1"]),
     ]
 
 
@@ -113,39 +117,56 @@ def test_create_fails_without_mandatory_fields(submission, indicators):
 
 @pytest.fixture
 def complex_indicator():
-    threat_actor = Entity.attribute("THREAT_ACTOR", "ActorName").set_valid_from(1604510497)\
-                                                                      .set_valid_to(1607102497)\
-                                                                      .set_confidence_score("LOW")
+    threat_actor = (
+        Entity.attribute("THREAT_ACTOR", "ActorName")
+        .set_valid_from(1604510497)
+        .set_valid_to(1607102497)
+        .set_confidence_score("LOW")
+    )
 
-    malware = Entity.attribute("MALWARE", "MalwareName").set_valid_from(1604510497)\
-                                                        .set_valid_to(1607102497)\
-                                                        .set_confidence_score("MEDIUM")
-    ip4 = Entity.observable("IP4", "2.2.2.2").set_valid_from(1604510497)\
-                                             .set_valid_to(1607102497)\
-                                             .set_confidence_score("LOW")
-    url = Entity.observable("URL", "wwww.relatedUrl.com").set_valid_from(1604510497)\
-                                                         .set_valid_to(1607102497)\
-                                                         .set_confidence_score("HIGH")
+    malware = (
+        Entity.attribute("MALWARE", "MalwareName")
+        .set_valid_from(1604510497)
+        .set_valid_to(1607102497)
+        .set_confidence_score("MEDIUM")
+    )
+    ip4 = (
+        Entity.observable("IP4", "2.2.2.2")
+        .set_valid_from(1604510497)
+        .set_valid_to(1607102497)
+        .set_confidence_score("LOW")
+    )
+    url = (
+        Entity.observable("URL", "wwww.relatedUrl.com")
+        .set_valid_from(1604510497)
+        .set_valid_to(1607102497)
+        .set_confidence_score("HIGH")
+    )
     indicator = [
-        Indicator("URL", "verybadurl").set_valid_from(1604510497).set_valid_to(1607102497)
-                                      .set_confidence_score("LOW").set_malicious_score("BENIGN")
-                                      .set_attributes([threat_actor, malware])
-                                      .set_related_observables([ip4, url])
-                                      .set_tags(["importantTag", "anotherTag"])
+        Indicator("URL", "verybadurl")
+        .set_valid_from(1604510497)
+        .set_valid_to(1607102497)
+        .set_confidence_score("LOW")
+        .set_malicious_score("BENIGN")
+        .set_attributes([threat_actor, malware])
+        .set_related_observables([ip4, url])
+        .set_tags(["importantTag", "anotherTag"])
     ]
     return indicator
 
 
 @pytest.fixture
 def full_submission(submission, complex_indicator):
-    return submission.set_title("Report, complex test")\
-                     .set_content_indicators(complex_indicator)\
-                     .set_enclave_id("c0f07a9f-76e4-48df-a0d4-c63ed2edccf0")\
-                     .set_external_id("external-1234")\
-                     .set_external_url("externalUrlValue")\
-                     .set_timestamp(1607102497)\
-                     .set_tags(["random_tag"])\
-                     .set_raw_content("blob of text")
+    return (
+        submission.set_title("Report, complex test")
+        .set_content_indicators(complex_indicator)
+        .set_enclave_id("c0f07a9f-76e4-48df-a0d4-c63ed2edccf0")
+        .set_external_id("external-1234")
+        .set_external_url("externalUrlValue")
+        .set_timestamp(1607102497)
+        .set_tags(["random_tag"])
+        .set_raw_content("blob of text")
+    )
 
 
 def test_submission_ok_json(full_submission):
