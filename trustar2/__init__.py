6f7fbe0906c5c43db0b16f2bd87ea7a678ce72f5
Hernan Tain <htain@trustar.co>
2021-04-13 17:16:59 -0300

    tests

--- trustar2/__init__.py
+++ trustar2/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 from trustar2.indicators import SearchIndicator
 from trustar2.submission import Submission
 from trustar2.trustar import TruStar
+from trustar2.safelist import Safelist
--- trustar2/safelist.py
+++ trustar2/safelist.py
@@ -50,11 +50,11 @@ class Safelist(object):
 
     def _verify_entry(self, entry):
         entity = entry.get("entity")
-        entity_type = entity.get("type")
+        entity_type = entry.get("type")
         if not entity or not entity_type:
             raise AttributeError("A new entry should have 'entity' and 'type' fields")
 
-        if not isinstance(entity, str):
+        if not isinstance(entity, type("")):
             raise AttributeError("Your entity value should be a string")
 
         obs_types = ObservableTypes.members()
@@ -74,20 +74,23 @@ class Safelist(object):
 
 
     def set_library_name(self, library_name):
-        if not isinstance(library_name, str):
+        if not isinstance(library_name, type("")):
             raise AttributeError("Library name must be a string.")
 
         self.set_custom_param("name", library_name)
 
     
-    def set_text(self, text):
-        # TODO: Validations
+    def set_text_to_be_extracted(self, text):
+        if not isinstance(text, type("")):
+            raise AttributeError("You can only submit a text for extraction.")
+
         self.set_custom_param("text", text)
 
 
     def _validate_library_guid_is_present(self):
         if self.library_guid is None:
             raise AttributeError("No library guid was found.")
+
 
     def get_safelist_libraries(self):
         return Query(self.config, self.summaries_endpoint, Methods.GET).set_params(self.params).fetch_one()
@@ -100,6 +103,11 @@ class Safelist(object):
 
     def create_entries(self):
         self._validate_library_guid_is_present()
+        if not self.params.get("entries"):
+            raise AttributeError(
+                "You must call the 'set_safelist_entries' method before calling this method."
+            )
+
         return Query(self.config, self.details_endpoint, Methods.PATCH).set_params(self.params).fetch_one()
 
 
@@ -124,4 +132,9 @@ class Safelist(object):
 
 
     def extract_terms(self):
+        if not self.params.get("text"):
+            raise AttributeError(
+                "You did not set any text for entities extraction. Call 'set_text_to_be_extracted' before."
+            )
+
         return Query(self.config, self.extract_endpoint, Methods.POST).set_params(self.params).fetch_one()
