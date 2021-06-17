d47ecf4df1c948ca4466fe710d5106f426b72490
Ivan Monaco <imonaco@splunk.com>
2021-06-17 17:12:14 -0300

    Imports at handlers init

--- trustar2/handlers/__init__.py
+++ trustar2/handlers/__init__.py
@@ -1,6 +1,7 @@
-from trustar2.handlers.tags import TagIndicator, TagSubmission
+from trustar2.handlers.tags import TagIndicator, TagSubmission, TagObservable
 from trustar2.handlers.indicators import SearchIndicator
 from trustar2.handlers.safelist import Safelist
 from trustar2.handlers.submission import Submission
 from trustar2.handlers.account import Account
 from trustar2.handlers.workflow import Workflow
+from trustar2.handlers.observables import ObservablesHandler
